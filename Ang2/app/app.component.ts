e1b482b1f31727fa318784a4ccca51b47b1e6eee
BuriakPavel <buriak.pavel@gmail.com>
2016-09-26 19:29:14 +0300

    test commit

--- Ang2/app/app.component.ts
+++ Ang2/app/app.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
     <ul>
     <li>1</li>
     <li>2</li>
-    <li>3</li>
+    <li>3
+        <ul>
+            <li>3.1</li>
+            <li>3.2</li>
+        </ul>
+        </li>
     <li>4</li>
+    <li>5
+        <ul>
+            <li>5.1</li>
+            <li>5.2</li>
+            <li>5.3</li>
+        </ul>
+    </li>
+    <li>6</li>
     </ul>`
 })
 export class AppComponent { }
