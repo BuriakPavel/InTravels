663b6962165512d6fb10c72c367ffa0d98f7933b
BuriakPavel <buriak.pavel@gmail.com>
2016-09-27 14:31:58 +0300

    added content folders, html template at index.html

--- Ang2/app/app.component.ts
+++ Ang2/app/app.component.ts
@@ -2,25 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>
-    <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3
-        <ul>
-            <li>3.1</li>
-            <li>3.2</li>
-        </ul>
-        </li>
-    <li>4</li>
-    <li>5
-        <ul>
-            <li>5.1</li>
-            <li>5.2</li>
-            <li>5.3</li>
-        </ul>
-    </li>
-    <li>6</li>
-    </ul>`
+    templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+    name: string;
+
+    constructor(){
+        this.name = "Username";
+    }
+}
