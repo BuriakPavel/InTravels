60e3563293bf94359944cef633b0cb3a96e25384
BuriakPavel <buriak.pavel@gmail.com>
2016-11-24 23:07:36 +0200

    added auto-detect for localization by user-browser settings

--- InTravels.WebAPI/Attributes/CultureAttribute.cs
+++ InTravels.WebAPI/Attributes/CultureAttribute.cs
@@ -20,17 +20,26 @@ namespace InTravels.Attributes
 			string cultureName = null;
 			// Получаем куки из контекста, которые могут содержать установленную культуру
 			HttpCookie cultureCookie = filterContext.HttpContext.Request.Cookies["lang"];
-			if (cultureCookie != null)
-				cultureName = cultureCookie.Value;
-			else
-				cultureName = "en"; //english by default
+            if (cultureCookie != null)
+                cultureName = cultureCookie.Value;
+            else
+            {
+                var userLanguages = filterContext.HttpContext.Request.UserLanguages;
+                CultureInfo ci;
+                if (userLanguages.Count() > 0)
+                {
+                    cultureName = userLanguages[0].Split('-')[0];
+                }
+            }
 
 			// Список культур
-			List<string> cultures = new List<string>() { "en", "ru", "uk-UA" }; // TODO! Replace to config file!
+			List<string> cultures = new List<string>() { "en", "ru", "uk" }; // TODO! Replace to config file!
 			if (!cultures.Contains(cultureName))
 			{
 				cultureName = "en"; //english by default
-			}
+            }
+
+            filterContext.HttpContext.Response.Cookies.Add(new HttpCookie("lang", cultureName));
 			Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
 			Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureName);
 		}
